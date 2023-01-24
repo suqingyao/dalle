e95e442eb549f92175298f37be8914de3a1a7134
cullyfung <cullyfung@gmail.com>
2023-01-24 14:00:26 +0800

    chore: handle create-post

--- client/src/pages/CreatePost.tsx
+++ client/src/pages/CreatePost.tsx
@@ -40,8 +40,30 @@ const CreatePost = () => {
     }
   }
 
-  const handleSubmit = (e: FormEvent) => {
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault()
+    if (form.prompt && form.photo) {
+      setLoading(true)
+
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(form)
+        })
+
+        await response.json()
+        navigate('/')
+      } catch (error) {
+        alert(error)
+      } finally {
+        setLoading(false)
+      }
+    } else {
+      alert('Please enter a prompt and generate an image')
+    }
   }
 
   const handleChange = (e: FormEvent) => {
